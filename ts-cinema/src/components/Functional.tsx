0e5f60d7a08c35d6aba6fc03c8ac1ff3e2e1533a
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-02-12 02:25:01 +0100

    feat: add and style main and func containers

---
+++ ts-cinema/src/components/Functional.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './myStyles.css'
+
+const Functional = () => {
+  return (
+    <div className='functional'>
+      
+    </div>
+  )
+}
+
+export default Functional
