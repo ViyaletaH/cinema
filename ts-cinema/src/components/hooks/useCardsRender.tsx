76e0e7c23cd693cd9cbe073da9d050dbea1e6f80
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-02-22 01:12:56 +0100

    fix: custom hook

--- ts-cinema/src/components/hooks/useCardsRender.tsx
+++ ts-cinema/src/components/hooks/useCardsRender.tsx
@@ -1,11 +1,9 @@
 
 function useCardsRender(arr:{}[], cont: Element): void {
-    if(document.readyState === 'complete') {
         for (let i = 0; i < arr.length; i++) {
         const card = document.createElement("div")!;
         card.classList.add("card");
         cont.appendChild(card);
-    }
 }
 
 }
