5df8f5d7745bf25ff6972673da7153d2c063719a
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-03-16 23:31:12 +0100

    feat: add basic slider cards styles

--- ts-cinema/src/components/Popular.tsx
+++ ts-cinema/src/components/Popular.tsx
@@ -1,17 +1,29 @@
-import './myStyles.css'
-import { sliderCards } from './data/sliderCards';
+import "./myStyles.css";
+import { sliderCards } from "./data/sliderCards";
 
 function Popular() {
-
   return (
-    <div className='popular'>
+    <div className="popular">
       {sliderCards.map((item) => (
-        <div className='card-popular' key={item.cardId}>
-          {item.popularity}
+        <div
+          className="card-popular"
+          key={item.cardId}
+          style={{
+            backgroundImage:
+              item.poster !== ""
+                ? `url( posters/${item.poster}.png)`
+                : `url(./logo.png)`,
+            backgroundPosition: "center",
+            backgroundSize: item.poster !== "" ? "cover" : "contain",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: item.poster !== "" ? undefined : "#fff",
+          }}
+        >
+          {/* {item.popularity} */}
         </div>
       ))}
     </div>
-  )
+  );
 }
 
-export default Popular
+export default Popular;
